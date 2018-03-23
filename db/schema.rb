d199d0f54b642b4d1b10c4a30c97ce7f9d4b6ff4
noureddine.brahmi <noureddine.brahmi@gmail.com>
2018-03-23 13:05:25 -0400

    After adding the jbuilder gem , resolving DB issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317082633) do
+ActiveRecord::Schema.define(version: 20180323165216) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
